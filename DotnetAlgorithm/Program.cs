6c31c25d385450849b81edf25cab7f831d0e969f
siyeon13 <siyeon0713@gmail.com>
2023-04-06 16:44:28 +0900

    Enter a message.

--- DotnetAlgorithm/Program.cs
+++ DotnetAlgorithm/Program.cs
@@ -11,6 +11,8 @@ namespace DotnetAlgorithm
         static void Main(string[] args)
         {
             // 알고리즘 문제 풀이 
+            // 백준 문제 
+            System.Console.ReadLine();
         }
     }
 }
